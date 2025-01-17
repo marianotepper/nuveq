239d686427ee2662f3d56db002469d93416963b8
Mariano Tepper <mariano.tepper@datastax.com>
2025-01-17 15:20:33 -0800

    Add subvectors

--- nuveq/nuveq.py
+++ nuveq/nuveq.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 import numpy as np
 
-from nes import ExponentialNES
+from .nes import ExponentialNES
 
 def quantize(x, n_bits):
     return np.round((2 ** n_bits - 1) * x) / (2 ** n_bits - 1)
@@ -102,12 +102,14 @@ class NVQParams:
 
 
 class NonuniformVectorQuantization:
-    def __init__(self, n_bits, nonlinearity='kumaraswamy',
+    def __init__(self, n_bits, n_subvectors=1,
+                 nonlinearity='kumaraswamy',
                  optimization_tol=1e-4,
                  optimization_max_iter=None,
                  optimization_seed=0,
                  store_optimization_loss_history=False):
         self.n_bits = n_bits
+        self.n_subvectors = n_subvectors
         self.nonlinearity = nonlinearity
         self.optimization_tol = optimization_tol
         self.optimization_max_iter = optimization_max_iter
@@ -131,45 +133,82 @@ class NonuniformVectorQuantization:
 
         return init_sol, bounds, sigma
 
-    def _get_loss_fun(self, data, x_min, x_max):
+    def _get_loss_fun(self, data, x_min, x_max, relative=True):
         baseline = loss_uniform(data, self.n_bits)
 
         if self.nonlinearity == 'kumaraswamy':
-            loss_fun = lambda sol: baseline / loss_kumaraswamy(sol, data,
-                                                               self.n_bits,
-                                                               x_min, x_max)
+            loss_fun = lambda sol: loss_kumaraswamy(
+                sol, data, self.n_bits, x_min, x_max
+            )
         elif self.nonlinearity == 'logistic':
-            loss_fun = lambda sol: baseline / loss_logistic(
-                sol, data, self.n_bits, x_min, x_max, logistic, logit)
+            loss_fun = lambda sol: loss_logistic(
+                sol, data, self.n_bits, x_min, x_max, logistic, logit
+            )
         elif self.nonlinearity == 'NQT':
-            loss_fun = lambda sol: baseline / loss_logistic(
-                sol, data, self.n_bits, x_min, x_max, logistic_nqt, logit_nqt)
+            loss_fun = lambda sol: loss_logistic(
+                sol, data, self.n_bits, x_min, x_max, logistic_nqt, logit_nqt
+            )
         else:
             raise ValueError('Unknown nonlinearity')
 
-        return loss_fun
+        if relative:
+            return lambda sol: baseline / loss_fun(sol)
+        else:
+            return loss_fun
+
+    def split_in_subvectors(self, data):
+        step = len(data) // self.n_subvectors
+        return [data[j:j + step] for j in np.arange(0, len(data), step)]
 
     def optimize(self, data):
-        x_min = data.min()
-        x_max = data.max()
+        nvq_params = []
 
-        loss_fun = self._get_loss_fun(data, x_min, x_max)
-        init_sol, bounds, sigma = self._get_init_state(x_min, x_max)
+        for subvector in self.split_in_subvectors(data):
+            x_min = subvector.min()
+            x_max = subvector.max()
 
-        xnes = ExponentialNES(tol=self.optimization_tol,
-                              max_iter=self.optimization_max_iter,
-                              seed=self.optimization_seed,
-                              distribution='separable')
-        result = xnes.optimize(loss_fun, init_sol, bounds=bounds,
-                               sigma=sigma,
-                               return_history=self.store_opt_loss_history)
-        if self.store_opt_loss_history:
-            xnes_sol, self.optimization_loss_history = result
+            loss_fun = self._get_loss_fun(subvector, x_min, x_max)
+            init_sol, bounds, sigma = self._get_init_state(x_min, x_max)
+
+            xnes = ExponentialNES(tol=self.optimization_tol,
+                                  max_iter=self.optimization_max_iter,
+                                  seed=self.optimization_seed,
+                                  distribution='separable')
+            result = xnes.optimize(loss_fun, init_sol, bounds=bounds,
+                                   sigma=sigma,
+                                   return_history=self.store_opt_loss_history)
+            if self.store_opt_loss_history:
+                xnes_sol, self.optimization_loss_history = result
+            else:
+                xnes_sol = result
+
+            nvq_params.append(NVQParams(x_min, x_max, xnes_sol))
+
+        if self.n_subvectors == 1:
+            return nvq_params[0], loss_fun(xnes_sol)
         else:
-            xnes_sol = result
+            return nvq_params, self.relative_loss(data, nvq_params)
 
-        return NVQParams(x_min, x_max, xnes_sol), loss_fun(xnes_sol)
 
     def loss(self, data, params):
-        loss_fun = self._get_loss_fun(data, params.x_min, params.x_max)
-        return loss_fun(params.distribution_params)
+        loss = 0
+        for subvector in self.split_in_subvectors(data):
+            loss_fun = self._get_loss_fun(subvector,
+                                          params.x_min, params.x_max,
+                                          relative=False)
+            loss += loss_fun(params.distribution_params)
+
+        return loss
+
+    def relative_loss(self, data, params):
+        baseline = loss_uniform(data, self.n_bits)
+
+        loss = 0
+        for subvector, par in zip(self.split_in_subvectors(data), params):
+            loss_fun = self._get_loss_fun(subvector,
+                                          par.x_min, par.x_max,
+                                          relative=False)
+            loss += loss_fun(par.distribution_params)
+
+        return baseline / loss
+
