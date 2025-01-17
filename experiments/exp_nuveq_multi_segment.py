2d6ebbec53831ecf1eaeddaf6012572da9d4dd1d
Mariano Tepper <mariano.tepper@datastax.com>
2025-01-17 15:21:57 -0800

    Test for multisegments

--- experiments/exp_nuveq_multi_segment.py
+++ experiments/exp_nuveq_multi_segment.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import plotly.express as px
 import plotly.io as pio
 import plotly.graph_objects as go
@@ -10,56 +11,43 @@ from datasets import select_dataset
 pio.templates.default = "plotly_white"
 
 
-def plot_nuveq_multi_vector(data):
-    segments = [1, 2, 4, 8]
-    n_bits = 8
+def plot_nuveq_multi_vector(data, n_bits):
+    ls_n_subvectors = [1, 2, 4, 8]
 
     print(f'{n_bits} bits')
 
-    ls_loss_kumaraswamy = dict([(s, []) for s in segments])
-    ls_loss_logistic = dict([(s, []) for s in segments])
-    ls_loss_nqt = dict([(s, []) for s in segments])
+    records = []
 
-    for i, vector in enumerate(data):
-        for nonlinearity, ls in [('logistic', ls_loss_logistic),
-                                 ('kumaraswamy', ls_loss_kumaraswamy),
-                                 ('NQT', ls_loss_nqt)]:
+    for nonlinearity, box_name in [('logistic', 'Log-Log'),
+                                   ('kumaraswamy', 'Kumaraswamy'),
+                                   ('NQT', 'NQT')]:
+        for ns in ls_n_subvectors:
             model = NonuniformVectorQuantization(n_bits,
+                                                 n_subvectors=ns,
                                                  nonlinearity=nonlinearity)
-            for n_segments in segments:
-                step = len(vector) // n_segments
-                loss_value_avg = 1
-                for j in np.arange(0, len(vector), len(vector) // n_segments):
-                    _, loss_value = model.optimize(vector[j:j+step])
-                    loss_value_avg *= loss_value
-                print(n_segments, np.pow(loss_value_avg, 1. / n_segments))
-            # ls.append(loss_value)
 
-    # print('Mean loss with Kumaraswamy nonlinearity',
-    #       np.mean(ls_loss_kumaraswamy))
-    # print('Mean loss with scaled Logistic nonlinearity',
-    #       np.mean(ls_loss_logistic))
-    # print('Mean loss with NQT nonlinearity',
-    #       np.mean(ls_loss_nqt))
+            for i, vector in enumerate(data):
+                _, loss_value = model.optimize(vector)
 
-    # fig = go.Figure(data=[
-    #     go.Box(name='Kumaraswamy', y=ls_loss_kumaraswamy,
-    #            boxpoints='outliers', showlegend=False),
-    #     go.Box(name='Log-Log', y=ls_loss_logistic,
-    #            boxpoints='outliers', showlegend=False),
-    #     go.Box(name='NQT', y=ls_loss_nqt,
-    #            boxpoints='outliers', showlegend=False),
-    # ])
-    # fig.add_hline(y=1, line=dict(dash='dash', color='black'))
-    # fig.add_annotation(text='Uniform',
-    #                    x=0.5, y=1, xref='paper',
-    #                    bgcolor='white',
-    #                    showarrow=False)
-    # fig.update_layout(
-    #     font=dict(size=18),
-    #     xaxis=dict(side='top'),
-    # )
-    # fig.show()
+                records.append(
+                    dict(Nonlinearity=box_name,
+                         Subvectors=ns,
+                         Loss=loss_value)
+                )
+
+    df = pd.DataFrame.from_records(records)
+
+    fig = px.box(df, x='Nonlinearity', y='Loss', color='Subvectors')
+    fig.add_hline(y=1, line=dict(dash='dash', color='black'))
+    fig.add_annotation(text='Uniform',
+                       x=0.5, y=1, xref='paper',
+                       bgcolor='white',
+                       showarrow=False)
+    fig.update_layout(
+        font=dict(size=20),
+        xaxis=dict(side='top'),
+    )
+    fig.show()
 
 
 def main():
@@ -74,7 +62,8 @@ def main():
 
     data -= np.mean(data, axis=0, keepdims=True)
 
-    plot_nuveq_multi_vector(data[:1])
+    plot_nuveq_multi_vector(data[:100], 4)
+    plot_nuveq_multi_vector(data[:100], 8)
 
 if __name__ == '__main__':
     main()
