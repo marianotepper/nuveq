5a96692db2460d021af05f38de1583e61c0f63d0
Mariano Tepper <mariano.tepper@datastax.com>
2025-02-26 10:50:08 -0800

    Update exports in the nuveq module

--- nuveq/__init__.py
+++ nuveq/__init__.py
@@ -1,1 +1,4 @@
-from .nuveq import NonuniformVectorQuantization, NVQParams, forward_kumaraswamy
+from .nuveq import (NonuniformVectorQuantization, NVQParams,
+                    forward_kumaraswamy, normalize, normalized_logistic,
+                    logistic, logit, logistic_nqt, logit_nqt,
+                    loss_logistic, loss_kumaraswamy, loss_uniform)
