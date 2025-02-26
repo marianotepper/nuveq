5285aa34cabc4879cab9278dc8719ca87910cfa5
Mariano Tepper <mariano.tepper@datastax.com>
2025-02-26 10:42:03 -0800

    Update scripts for saving the plots

--- experiments/exp_multi_vector.py
+++ experiments/exp_multi_vector.py
@@ -6,11 +6,18 @@ import sys
 
 from nuveq import NonuniformVectorQuantization
 from datasets import select_dataset
+from plot_utils import write_image
 
 pio.templates.default = "plotly_white"
 
 
-def plot_nuveq_multi_vector(data):
+def plot_nuveq_multi_vector(dirname, dataset_name, n_samples=10_000):
+    dataset = select_dataset(dirname, dataset_name)
+
+    data = dataset.X_db
+    data -= np.mean(data, axis=0, keepdims=True)
+    data = data[:n_samples]
+
     n_bits = 8
 
     print(f'{n_bits} bits')
@@ -51,12 +58,17 @@ def plot_nuveq_multi_vector(data):
     fig.add_annotation(text='Uniform', x=1, y=1,
                        arrowwidth=2,
                        showarrow=True, arrowhead=3)
-    fig.update_xaxes(title_text='Loss with Kumaraswamy nonlinearity')
-    fig.update_yaxes(title_text='Loss with Log-Log nonlinearity')
+    fig.update_xaxes(title_text='Loss with Kumaraswamy')
+    fig.update_yaxes(title_text='Loss with Log-Log')
     fig.update_layout(
-        font=dict(size=18),
+        legend=dict(x=0.5, y=1.02, orientation="h",
+                    yanchor="bottom", xanchor="center"),
+        font=dict(size=25),
+        autosize=True,
+        margin={'l': 0, 'r': 0, 't': 0, 'b': 0},
     )
     fig.show()
+    write_image(fig, f'multi_vector_kuma_vs_logistic_{dataset.name}.pdf')
 
     fig = go.Figure(data=[
         go.Scatter(name='vectors',
@@ -69,12 +81,17 @@ def plot_nuveq_multi_vector(data):
     fig.add_annotation(text='Uniform', x=1, y=1,
                        arrowwidth=2,
                        showarrow=True, arrowhead=3)
-    fig.update_xaxes(title_text='Loss with Log-Log nonlinearity')
-    fig.update_yaxes(title_text='Loss with NQT nonlinearity')
+    fig.update_xaxes(title_text='Loss with Log-Log')
+    fig.update_yaxes(title_text='Loss with NQT')
     fig.update_layout(
-        font=dict(size=18),
+        legend=dict(x=0.5, y=1.02, orientation="h",
+                    yanchor="bottom", xanchor="center"),
+        font=dict(size=25),
+        autosize=True,
+        margin={'l': 0, 'r': 0, 't': 0, 'b': 0},
     )
     fig.show()
+    write_image(fig, f'multi_vector_logistic_vs_nqt_{dataset.name}.pdf')
 
     fig = go.Figure(data=[
         go.Box(name='Kumaraswamy', y=ls_loss_kumaraswamy,
@@ -90,25 +107,21 @@ def plot_nuveq_multi_vector(data):
                        bgcolor='white',
                        showarrow=False)
     fig.update_layout(
-        font=dict(size=18),
+        font=dict(size=30),
         xaxis=dict(side='top'),
+        autosize=True,
+        margin={'l': 0, 'r': 0, 't': 0, 'b': 0},
     )
     fig.show()
+    write_image(fig, f'multi_vector_improvement_{dataset.name}.pdf')
 
 
 def main():
     dirname = sys.argv[1]
-    dataset = select_dataset(dirname, 'ada002-100k')
-    # dataset = select_dataset(dirname, 'openai-v3-small-100k')
-    # dataset = select_dataset(dirname, 'gecko-100k')
-    # dataset = select_dataset(dirname, 'nv-qa-v4-100k')
-    # dataset = select_dataset(dirname, 'colbert-1M')
 
-    data = dataset.X_db
+    plot_nuveq_multi_vector(dirname, 'ada002-100k',
+                            n_samples=10_000)
 
-    data -= np.mean(data, axis=0, keepdims=True)
-
-    plot_nuveq_multi_vector(data[:10_000])
 
 if __name__ == '__main__':
     main()
