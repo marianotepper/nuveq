b2c09fb1c1ec5fe001b46e2af51119ffef41956e
Mariano Tepper <mariano.tepper@datastax.com>
2025-03-04 15:14:47 -0800

    Add plots of the parameters of the scaled logistic/logit nonlinearities

---
+++ experiments/exp_logistic_params.py
@@ -0,0 +1,74 @@
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly.io as pio
+from plotly.subplots import make_subplots
+
+from nuveq import normalized_logistic, normalized_logit, logistic, logit
+from plot_utils import write_image
+
+pio.templates.default = "plotly_white"
+
+x = np.linspace(0, 1, 1_000, endpoint=True)
+
+palette = px.colors.qualitative.Plotly
+
+fig = make_subplots(rows=1, cols=2, subplot_titles=('Logistic',  'Logit'))
+
+for i, (alpha, x0, dash) in enumerate([(1, 0.5, 'solid'),
+                                       (7.5, 0.5, 'dot'), (15, 0.5, 'dashdot'),
+                                       (10, 0.35, 'dot'), (10, 0.65, 'dashdot')]):
+    bias = logistic(0, alpha, x0)
+    scale = logistic(1, alpha, x0) - bias
+
+    str_alpha = f'{alpha}'
+    str_x0 = f'{x0}'
+    name = r'$\huge{\alpha=' + str_alpha + ', x_0=' + str_x0 + '}$'
+
+    fig.add_trace(
+        go.Scatter(name=name,
+                   x=x, y=normalized_logistic(logistic, x, alpha, x0, scale, bias),
+                   line=dict(dash=dash, color=palette[i], width=5),
+                   showlegend=True,
+                   mode='lines'),
+        row=1, col=1
+    )
+
+    fig.add_trace(
+        go.Scatter(name=name,
+                   x=x, y=normalized_logit(logit, x, alpha, x0, scale, bias),
+                   line=dict(dash=dash, color=palette[i], width=5),
+                   showlegend=False,
+                   mode='lines'),
+        row=1, col=2
+    )
+
+fig.update_xaxes(range=[-0.05, 1.05], row=1, col=1)
+fig.update_xaxes(range=[-0.05, 1.05], row=1, col=2)
+
+fig.update_yaxes(range=[-0.05, 1.05], row=1, col=1)
+fig.update_yaxes(range=[-0.05, 1.05], row=1, col=2)
+
+fig.update_annotations(font_size=25)
+
+fig.update_layout(
+    height=600,
+    width=1200,
+    xaxis1=dict(
+        tickfont=dict(size=25)),
+    yaxis1= dict(
+        tickfont=dict(size=25)),
+    xaxis2=dict(
+        tickfont=dict(size=25)),
+    yaxis2=dict(
+        tickfont=dict(size=25)),
+    autosize=True,
+    margin={'l': 0, 'r': 0, 't': 0, 'b': 0},
+    legend=dict(
+        yanchor="top",
+        y=0.99,
+        xanchor="left",
+        x=0.01),
+)
+fig.show()
+write_image(fig, 'logistic_parameter_examples.png', scale=3)
--- nuveq/__init__.py
+++ nuveq/__init__.py
@@ -1,4 +1,5 @@
 from .nuveq import (NonuniformVectorQuantization, NVQParams,
-                    forward_kumaraswamy, normalize, normalized_logistic,
+                    forward_kumaraswamy, normalize,
+                    normalized_logistic, normalized_logit,
                     logistic, logit, logistic_nqt, logit_nqt,
                     loss_logistic, loss_kumaraswamy, loss_uniform)
